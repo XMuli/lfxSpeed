2e9cab03a0706252051730c91a7b7b61986b41a6
xmuli <xmulitech@gmail.com>
2020-11-20 09:40:54 +0800

    docs: Adding MIT License Notices to Source Code

--- speedplugin.h
+++ speedplugin.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2020 xmuli
+ *
+ * Author:  xmuli(偕臧) xmulitech@gmail.com
+ * GitHub:  https://github.com/xmuli
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * */
+
 #ifndef SPEEDPLUGIN_H
 #define SPEEDPLUGIN_H
 
