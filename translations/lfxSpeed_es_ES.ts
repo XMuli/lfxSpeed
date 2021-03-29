63f42978322f155e7f42fe3c1ad202fe6556d4b3
alvarosamudio <alvarosamudio@criptext.com>
2021-03-29 12:59:38 -0500

    i18n implementation

---
+++ translations/lfxSpeed_es_ES.ts
@@ -0,0 +1,174 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../aboutdialog.cpp" line="35"/>
+        <source>lfxSpeed</source>
+        <translation type="unfinished">lfxSpeed</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.cpp" line="36"/>
+        <source>A light and fast network speed plugin(DDE).</source>
+        <translation type="unfinished">Un plugin que monitorea la velocidad de red ligero y rápido (DDE).</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.cpp" line="48"/>
+        <source>Version: 0.6.4  2021/03/25</source>
+        <translation type="unfinished">Versión: 0.6.4  2021/03/25</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.cpp" line="49"/>
+        <source>Copyright (c) 2021 By ifmet.cn</source>
+        <translation type="unfinished">Copyright (c) 2021 por ifmet.cn</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.cpp" line="58"/>
+        <source>&lt;a href=&quot;https://ifmet.cn&quot;&gt;Author</source>
+        <translation type="unfinished">&lt;a href=&quot;https://ifmet.cn&quot;&gt;Autor</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.cpp" line="61"/>
+        <source>&lt;a href=&quot;https://github.com/xmuli/lfxSpeed&quot;&gt;GitHub</source>
+        <translation type="unfinished">&lt;a href=&quot;https://github.com/xmuli/lfxSpeed&quot;&gt;GitHub</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.cpp" line="64"/>
+        <source>&lt;a href=&quot;https://github.com/xmuli/lfxSpeed/blob/master/LICENSE&quot;&gt;License</source>
+        <translation type="unfinished">&lt;a href=&quot;https://github.com/xmuli/lfxSpeed/blob/master/LICENSE&quot;&gt;Licencia</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.cpp" line="67"/>
+        <source>&lt;a href=&quot;&quot;&gt;Donation</source>
+        <translation type="unfinished">&lt;a href=&quot;&quot;&gt;Donación</translation>
+    </message>
+    <message>
+        <location filename="../aboutdialog.cpp" line="70"/>
+        <source>&lt;a href=https://github.com/justforlxz&quot;&gt;Thanks</source>
+        <translation type="unfinished">&lt;a href=https://github.com/justforlxz&quot;&gt;Agradecimientos:</translation>
+    </message>
+</context>
+<context>
+    <name>SpeedInfo</name>
+    <message>
+        <location filename="../speedinfo.cpp" line="289"/>
+        <source>系统已运行: %1天, %2:%3:%4</source>
+        <translation type="unfinished">El sistema es operativo: %1día, %2:%3:%4</translation>
+    </message>
+</context>
+<context>
+    <name>SpeedWidget</name>
+    <message>
+        <location filename="../speedwidget.cpp" line="97"/>
+        <location filename="../speedwidget.cpp" line="99"/>
+        <source>0 Kb/s</source>
+        <translation type="unfinished">0 Kb/s</translation>
+    </message>
+    <message>
+        <location filename="../speedwidget.cpp" line="101"/>
+        <location filename="../speedwidget.cpp" line="103"/>
+        <source>0 %</source>
+        <translation type="unfinished">0 %</translation>
+    </message>
+</context>
+<context>
+    <name>WinMain</name>
+    <message>
+        <location filename="../winmain.cpp" line="86"/>
+        <source>显示文本:</source>
+        <translation type="unfinished">Mostrar texto:</translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="88"/>
+        <source>上传:</source>
+        <translation type="unfinished">Subida:</translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="89"/>
+        <source>↑:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="91"/>
+        <source>下载:</source>
+        <translation type="unfinished">Descarga:</translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="92"/>
+        <source>↓:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="94"/>
+        <location filename="../winmain.cpp" line="95"/>
+        <source>CPU:</source>
+        <translation type="unfinished">CPU:</translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="97"/>
+        <source>内存:</source>
+        <translation type="unfinished">RAM:</translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="98"/>
+        <source> MEM:</source>
+        <translation type="unfinished">MEM:</translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="104"/>
+        <source>小数精确度:</source>
+        <translation type="unfinished">Decimales:</translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="109"/>
+        <source>刷新时隔:</source>
+        <translation type="unfinished">Intervalo de actualización:</translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="113"/>
+        <source>ms</source>
+        <translation type="unfinished">ms</translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="115"/>
+        <source>单位模式:</source>
+        <translation type="unfinished">Modelo de unidad:</translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="125"/>
+        <source>显示上传网速</source>
+        <translation type="unfinished">Mostrar la velocidad de Subida</translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="128"/>
+        <source>显示下载网速</source>
+        <translation type="unfinished">Mostrar la velocidad de Bajada</translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="131"/>
+        <source>显示CPU</source>
+        <translation type="unfinished">Mostrar CPU</translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="134"/>
+        <source>显示内存</source>
+        <translation type="unfinished">Mostrar MEM</translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="137"/>
+        <source>显示鼠标提示</source>
+        <translation type="unfinished">Mostrar las señales del ratón</translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="140"/>
+        <source>上传下载互换</source>
+        <translation type="unfinished">Intercambiar Carga y Descarga</translation>
+    </message>
+    <message>
+        <location filename="../winmain.cpp" line="151"/>
+        <source>任务栏窗口设置</source>
+        <translation type="unfinished">Configuración de la ventana en la barra de tareas</translation>
+    </message>
+</context>
+</TS>
